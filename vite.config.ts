7fbee03448f29cbc311514d263f79d97cbdee262
Your Name <leo.lbdias@gmail.com>
2023-02-14 17:25:37 -0300

    chore: :heavy_plus_sign: adding vercel-adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import solid from "solid-start/vite";
+import vercel from "solid-start-vercel";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid({ ssr: false })],
+  plugins: [solid({ adapter: vercel({}) })],
   ssr: { external: ["@prisma/client"] },
 });
